2821cd96c4e08702af2220712a9b670b923b88da
YoshinoriN <nayo-msoft@outlook.jp>
2016-10-05 11:58:02 +0900

    Create redmine-download.sh

---
+++ Applications/Redmine/3.3.0/rails/redmine-download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+REDMINE_VERSION="3.3.0"
+MD5_CHECKSUM="0c0abb2d4efde455c3505d8caf01cb2d"
+
+curl -O -s http://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
+
+line=`md5sum redmine-${REDMINE_VERSION}.tar.gz`
+set -f
+set -- $line
+
+if [ $MD5_CHECKSUM = $1 ];then
+    tar xvfz redmine-${REDMINE_VERSION}.tar.gz
+else
+  echo "doesn't match the check sum."
+fi
