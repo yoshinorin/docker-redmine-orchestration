bee2fca7e4c695a796280f7e25a25659156e12e5
YoshinoriN <nayo-msoft@outlook.jp>
2016-10-08 17:22:44 +0900

    Update unicorn.rb

--- Applications/Redmine/3.3.0/rails/config/unicorn.rb
+++ Applications/Redmine/3.3.0/rails/config/unicorn.rb
@@ -1,5 +1,33 @@
-worker_processes 1
-listen  "3000" 
-pid     '/tmp/unicorn.pid'
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+rails_root = File.expand_path('../../', __FILE__)
+
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+worker_processes 2
+
+timeout 60
+
+stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+
+listen "#{rails_root}/tmp/sockets/unicorn.sock", :backlog => 1024
+
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
