ada6a121913ec66400f1bad3e7dfed449621344d
YoshinoriN <nayo-msoft@outlook.jp>
2016-10-12 08:11:49 +0000

    Change directory hierarchy and some settings

--- Applications/Redmine/3.3/redmine/config.default/README.md
+++
@@ -1,1 +0,0 @@
-These yaml are default settings of Redmine 3.3.0.
--- Applications/Redmine/3.3/redmine/redmine_download_main.sh
+++ Applications/Redmine/3.3/redmine/redmine_download_main.sh
@@ -3,6 +3,8 @@
 REDMINE_VERSION=$1
 MD5_CHECKSUM=$2
 
+echo "[INFO] Downloading..."
+
 curl -O -s http://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
 
 line=`md5sum redmine-${REDMINE_VERSION}.tar.gz`
@@ -11,11 +13,12 @@ set -- $line
 
 if [ $MD5_CHECKSUM = $1 ];then
   tar xvfz redmine-${REDMINE_VERSION}.tar.gz
-  mv redmine-${REDMINE_VERSION} redmine
+  cp -Rf redmine-${REDMINE_VERSION}/. redmine
+  rm -rf redmine-${REDMINE_VERSION}
   rm -rf redmine/redmine-${REDMINE_VERSION}
   rm redmine-${REDMINE_VERSION}.tar.gz
 else
-  echo "doesn't match the check sum."
+  echo "[WARN] Doesn't match the check sum."
 fi
 
-echo "Downloaded."
+echo "[INFO] Finish."
