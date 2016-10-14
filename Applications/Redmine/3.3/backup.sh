6664d12549f55c1f3167b40ac04cc85b2a6c9c8f
YoshinoriN <nayo-msoft@outlook.jp>
2016-10-14 04:37:26 +0000

    Fix backup script

--- Applications/Redmine/3.3/backup.sh
+++ Applications/Redmine/3.3/backup.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 set -e
 
-BACKUP_ROOT_DIR_NAME=backups
+ROOT_DIR_NAME=backups
+SUB_DIR_NAME=`date +%Y%m%d`
+FILE_PREFIX=`date +%H%M%S`
 
 echo "[INFO] Start backup."
 
-if [ ! -d ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d` ]; then
-  mkdir ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`
+if [ ! -d ./${ROOT_DIR_NAME}/${SUB_DIR_NAME} ]; then
+  mkdir ./${ROOT_DIR_NAME}/${SUB_DIR_NAME}
 fi
 
 # TODO : Can create safe backup option (Stop all containers befor backup).
-docker export redmine-files > ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_data_redmine_files.tar.gz
-docker export mariadb-storage > ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_data_mariadb.tar.gz
+docker export redmine-files > ./${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_data_redmine_files.tar.gz
+docker export mariadb-storage > ./${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_data_mariadb.tar.gz
 
-if [ -n $1 ]; then
-  echo "[INFO] Application Container backup."
-  if [ $1 = "-f" ]; then
-    docker export redmine-files > ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_nginx.tar.gz
-    docker export mariadb-storage > ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_mariadb.tar.gz
-    docker export mariadb-storage > ./${BACKUP_ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_redmine.tar.gz
-  else
-    echo "[ERROR] Argument1 is error."
-    echo "-f : full backup."
-  fi
-fi
+docker run --volumes-from mariadb-storage -v $(pwd)/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/:/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/ \
+  busybox:uclibc tar cvf /${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_data_mariadb.tar /var/lib/mysql
+
+docker run --volumes-from redmine-files -v $(pwd)/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/:/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/ \
+  busybox:uclibc tar cvf /${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_files_redmine.tar /usr/src/app/redmine/files
+
+#if [ -n $1 ]; then
+#  echo "[INFO] Application Container backup."
+#  if [ $1 = "-f" ]; then
+#    docker export redmine-files > ./${ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_nginx.tar.gz
+#    docker export mariadb-storage > ./${ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_mariadb.tar.gz
+#    docker export mariadb-storage > ./${ROOT_DIR_NAME}/`date +%Y%m%d`/`date +%Y%m%d_%H%M%S`_app_redmine.tar.gz
+#  else
+#    echo "[ERROR] Argument1 is error."
+#    echo "-f : full backup."
+#  fi
+#fi
 
 echo "[INFO] Finish backup."
