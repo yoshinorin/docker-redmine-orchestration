e5c4e8bed535d72ffba42a9a08ca6b83f92eb7b7
YoshinoriN <nayo-msoft@outlook.jp>
2017-07-14 17:58:45 +0900

    (refs #34) : Bumpt to Redmine 3.4.1

--- redmine/redmine/redmine_3.4.1_download.sh
+++ redmine/redmine/redmine_3.4.1_download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-REDMINE_VERSION="3.3.2"
-MD5_CHECKSUM="8e403981dc3a19a42ee978f055be62ca"
+REDMINE_VERSION="3.4.1"
+MD5_CHECKSUM="79b07289c0b591e81180d017dbf6ebf4"
 
 cd $(dirname $0)
 echo pwd
