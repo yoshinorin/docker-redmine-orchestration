fce7a6fbc8595ae7ef3bb6136524f3bdb21303b2
yoshinorin <yoshinorin.net@outlook.com>
2021-04-24 07:42:38 +0900

    chore: delete unnecessary echo command

--- redmine/redmine_download.sh
+++ redmine/redmine_download.sh
@@ -6,7 +6,6 @@ SHA256_CHECKSUM="05faafe764330f2d77b0aacddf9d8ddce579c3d26bb8e03a7d6e7ff461f1cdd
 echo "[INFO] Downloading..."
 
 cd $(dirname $0)
-echo pwd
 
 curl -O -s https://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
 
